9039a0b9545ffb9a90817a4c0eff64d7d5c7de0f
Alexandru Garbacea <g99.alex@yahoo.com>
2022-12-19 17:06:17 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [speed, setSpeed] = useState(0);
+  const [speedLimit, setSpeedLimit] = useState(130);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='mid-page'>
+        <div className='main-circle'>
+          <div className={`${+speed > +speedLimit ? 'warning' : ''}`}>{speed}</div>
+          <div className='measurment'>km/h</div>
+          <div className={`speed-circle${+speed > +speedLimit ? ' on' : ''}`}>{speedLimit}</div>
+        </div>
+      </section>
+      <section className='controls'>
+        <div>
+          <label>Speed({speed})</label>
+          <input 
+            type='range' min={0} max={180} id='speed-slider' value={speed} 
+            onChange={(e) => setSpeed(e.target.value)} 
+          />
+          <label>Speed limit({speedLimit})</label>
+          <input 
+            type='range' min={10} max={130} id='speed-limit-slider' value={speedLimit} step={5}
+            onChange={(e) => setSpeedLimit(e.target.value)} 
+          />
+        </div>
+      </section>
     </div>
   );
 }
